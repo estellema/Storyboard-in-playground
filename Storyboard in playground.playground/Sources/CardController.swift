45e08829534fb22486054da0223212a7694c40d0
Lyee <3509470364@qq.com>
2020-10-24 14:26:23 +0800

    first commit

---
+++ Storyboard in playground.playground/Sources/CardController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+open class CardController: UIViewController {
+    @IBOutlet public var card: UIView!
+    @IBOutlet public var closeButton: UIButton!
+
+    @IBOutlet public var CardController: Array<NSLayoutConstraint>!
+
+    @IBAction open func tapCard (_ sender: AnyObject) {}
+    @IBAction open func tapClose (_ sender: AnyObject) {}
+
+}
+
